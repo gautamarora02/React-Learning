6daf2703454dae8fc33f27e154b8c3523a7d92a5
gautamarora04 <gautamvijayarora@gmail.com>
2024-07-12 00:10:32 +0530

    Inital commit

---
+++ 01basicreact/src/Youtube.js
@@ -0,0 +1,6 @@
+function Youtube(){
+    return (
+        <h1>Youtube App CRA</h1>
+    )
+}
+export default Youtube;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import Youtube from './Youtube'
+function App() {
+  return (
+    <>
+    <h1>React Vite App</h1>
+    <h1>React Vite App</h1>
+    <Youtube/>
+    </>
+  )
+}
+
+export default App
